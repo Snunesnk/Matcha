877a390ee6f501ef14578a146017f0679470977a
Simon NUNES <snunes@student.42.fr>
2022-09-20 18:34:38 +0200

    Continue to style onboarding input

---
+++ src/Components/FormInput/FormInput.jsx
@@ -0,0 +1,9 @@
+import "./FormInput.css"
+
+const FormInput = ({ placeholder }) => {
+    return (
+        <input className="form_input" placeholder={placeholder}></input>
+    )
+}
+
+export default FormInput
--- src/Components/Onboarding/Onboarding.jsx
+++ src/Components/Onboarding/Onboarding.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Grid } from "@mui/material";
+import FormInput from "../FormInput/FormInput";
+import MainButton from '../MainButton/MainBtn';
 import './Onboarding.css'
 
 export default Onboarding => {
@@ -12,19 +14,17 @@ export default Onboarding => {
 
     return (
         <Grid container id='onboarding' className={onboardingClass}>
-            <Grid item xs={6} id="cat_pic"></Grid>
-            <Grid item xs={6} id="onboarding_content" className="centered_container">
+            <Grid item xs={5} id="cat_pic"></Grid>
+            <Grid item xs={7} id="onboarding_content" className="centered_container">
                 {/* <div id="has_account">Already have an account ?</div> */}
                 <Grid container id="form" className={onboardingClass}>
-                    <Grid item xs={12} className="centered_container">Matcha</Grid>
-                    <Grid item xs={12} className="centered_container">Find your catmate</Grid>
-                    <Grid item xs={12} className="centered_container"><input></input></Grid>
-                    <Grid item xs={12} className="centered_container"><input></input></Grid>
-                    <Grid item xs={12} className="centered_container"><input></input></Grid>
-                    <Grid item xs={12} className="centered_container"><input></input></Grid>
-                    <Grid item xs={12} className="centered_container"><input></input></Grid>
-                    <Grid item xs={12} className="centered_container"><button>Validate</button></Grid>
-
+                    <Grid item xs={12} className="centered_container catch_phrase center_align"><h2>Create your account to find your catmate</h2></Grid>
+                    <Grid item xs={12} className="centered_container input_container"><FormInput placeholder="First Name"></FormInput></Grid>
+                    <Grid item xs={12} className="centered_container input_container"><FormInput placeholder="Last Name"></FormInput></Grid>
+                    <Grid item xs={12} className="centered_container input_container"><FormInput placeholder="Username"></FormInput></Grid>
+                    <Grid item xs={12} className="centered_container input_container"><FormInput placeholder="Email"></FormInput></Grid>
+                    <Grid item xs={12} className="centered_container input_container"><FormInput placeholder="Password"></FormInput></Grid>
+                    <Grid item xs={12} className="centered_container input_container"><MainButton></MainButton></Grid>
                 </Grid>
             </Grid>
         </Grid>
