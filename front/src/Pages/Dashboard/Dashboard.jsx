76902b026f63cdc2b4bdd6a95808ed21e1f5e85f
Simon NUNES <snunes@student.42.fr>
2022-11-15 19:17:23 +0100

    Begin dashboard UI

--- front/src/Pages/Dashboard/Dashboard.jsx
+++ front/src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import DashboardFooter from "../../Components/DashboardFooter";
+
 import './Dashboard.css';
 
-const Dashboard = () => {
 
+const Dashboard = () => {
     return (
         <div id="dashboard">
-
+            <DashboardFooter />
         </div >
     )
 }
