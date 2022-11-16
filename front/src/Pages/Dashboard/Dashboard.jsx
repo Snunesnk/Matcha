9a9143a0dcef03a0517a70a472eba9e88626fd4d
Simon NUNES <snunes@student.42.fr>
2022-11-16 15:22:47 +0100

    Added links to navbar + begin to create profile cards

--- front/src/Pages/Dashboard/Dashboard.jsx
+++ front/src/Pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import DashboardFooter from "../../Components/DashboardFooter";
+import DashboardOverview from "../../Components/DashboardOverview";
 
 import './Dashboard.css';
 
@@ -7,6 +9,9 @@ import './Dashboard.css';
 const Dashboard = () => {
     return (
         <div id="dashboard">
+            <Routes>
+                <Route path="overview" element={<DashboardOverview />}></Route>
+            </Routes>
             <DashboardFooter />
         </div >
     )
