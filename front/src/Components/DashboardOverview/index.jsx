f51f8f0fcba078d377f8ddc3592a4f8a30ea1ae1
Simon NUNES <snunes@student.42.fr>
2022-11-17 17:32:47 +0100

    Continue dashboard

--- front/src/Components/DashboardOverview/index.jsx
+++ front/src/Components/DashboardOverview/index.jsx
@@ -1,13 +1,56 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import AdvancedSearch from "../AdvancedSearch";
 import ProfileCard from "../ProfileCard";
 import './index.css';
 
 const DashboardOverview = () => {
-    return (
-        <div>
-            <div className="card_container">
+    let results = [];
+
+    while (results.length < 25) {
+        results.push((
+            <Grid item className="card_container">
                 <ProfileCard />
-            </div>
+            </Grid>
+        ));
+
+    }
+
+    return (
+        // <div className="dashboard_overview">
+        //     <Grid item xs={4}>
+        //         <div>Age</div>
+        //     </Grid>
+        //     <Grid item xs={8} container>
+        //         <div className="cards_header">
+        //             <div className="overview_top_phrase">
+        //                 Profiles that you might like
+        //             </div>
+        //             <div className="overview_sort">
+        //                 Sort by
+        //             </div>
+        //         </div>
+        //         <div className="overview_grid_container">
+        //             <Grid container  >
+        //                 {results}
+        //             </Grid>
+        //         </div>
+        //     </Grid>
+        // </div >
+
+        <div className="dashboard_overview">
+            <Grid container>
+                <Grid item xs={4}>
+                    <AdvancedSearch />
+                </Grid>
+                <Grid item xs={4}>
+                    Profile Matching
+                </Grid>
+                <Grid item xs={4}>
+                    Messages
+                </Grid>
+            </Grid>
+
         </div>
     )
 }
