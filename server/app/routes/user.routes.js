3d08fb370409c60f8f8605899d18c5d483581810
Bastien RICHARD-CANAVAGGIO <brichard@e3r12p7.clusters.42paris.fr>
2022-11-18 17:58:51 +0100

    user-crud - setup vscode breakpoints for matcha_server
    env-file - Added env/ directory with docker-compose file and dev ops scripts
    
    user-crud - Self made ORM - user crud - user login check endpoint

--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -1,28 +1,27 @@
-module.exports = app => {
-    const user = require("../handlers/user.handler.js");
-  
-    var router = require("express").Router();
-  
-    // Create a new User
-    router.post("/", user.create);
-  
-    // Retrieve all Users
-    router.get("/", user.findAll);
-  
-    // Retrieve all verified Users
-    router.get("/verified", user.findAllVerified);
-  
-    // Retrieve a single User with login
-    router.get("/:login", user.findOne);
-  
-    // Update a User with login
-    router.put("/:login", user.update);
-  
-    // Delete a User with login
-    router.delete("/:login", user.delete);
-  
-    // Delete all Users
-    router.delete("/", user.deleteAll);
-  
-    app.use('/api/user', router);
-  };
+import express from "express";
+import userHandler from "../handlers/user.handler.js"
+
+const router = express.Router();
+
+// Login
+router.post("/user/login", userHandler.login);
+
+// Create a new User
+router.post("/user/", userHandler.create);
+
+// Retrieve all Users
+router.get("/user/", userHandler.getAllUsers);
+
+// Retrieve all verified Users
+router.get("/user/verified", userHandler.getAllVerified);
+
+// Retrieve a single User with login
+router.get("/user/login/:login", userHandler.getUserByLogin);
+
+// Update a User with login
+router.put("/user/login/:login", userHandler.update);
+
+// Delete a User with login
+router.delete("/user/login/:login", userHandler.delete);
+
+export default router
