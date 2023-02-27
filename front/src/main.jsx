9843f73012200b204c3cdfad7dc2a5cd40188cee
Simon NUNES <snunes@student.42.fr>
2023-02-27 13:29:53 +0100

    Trying  to correctly add everything

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from './Router/router'
-import Navbar from './Components/Navbar/Navbar'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
-        <Navbar />
         <RouterProvider router={router} />
-        {/* <Footer /> */}
     </React.StrictMode>
 )
