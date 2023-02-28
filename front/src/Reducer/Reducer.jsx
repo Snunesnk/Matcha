28ef73f67b86db6b0c369087d477c186aaaaf618
Simon NUNES <snunes@student.42.fr>
2023-02-28 15:07:18 +0100

    Continue trying to implement email verification

--- front/src/Reducer/Reducer.jsx
+++ front/src/Reducer/Reducer.jsx
@@ -1,42 +1,41 @@
-import { PossibleState } from "../constants";
+import { PossibleState } from '../constants'
 
 export const reducer = (state, action) => {
-    if (action.type === state.currentState)
-        return state;
+    if (action.type === state.currentState) return state
 
     switch (action.type) {
         case PossibleState.landingPage:
             return {
                 ...state,
                 currentState: PossibleState.landingPage,
-                navbarClass: "visible",
-                landingClass: "visible",
-                onboardingClass: "hidden",
-                dashboardClass: "hidden",
-            };
+                navbarClass: 'visible',
+                landingClass: 'visible',
+                onboardingClass: 'hidden',
+                dashboardClass: 'hidden',
+            }
 
         case PossibleState.onboarding:
             return {
                 ...state,
                 currentState: PossibleState.onboarding,
-                navbarClass: "hidden",
-                landingClass: "hidden",
-                onboardingClass: "visible",
-                dashboardClass: "hidden",
-            };
+                navbarClass: 'hidden',
+                landingClass: 'hidden',
+                onboardingClass: 'visible',
+                dashboardClass: 'hidden',
+            }
 
         case PossibleState.onboarding:
             return {
                 ...state,
                 currentState: PossibleState.onboarding,
-                navbarClass: "hidden",
-                landingClass: "hidden",
-                onboardingClass: "hidden",
-                dashboardClass: "visible",
-            };
+                navbarClass: 'hidden',
+                landingClass: 'hidden',
+                onboardingClass: 'hidden',
+                dashboardClass: 'visible',
+            }
 
-        case "user:updateUserInfos":
-            console.log(action);
+        case 'user:updateUserInfos':
+            console.log(action)
             return {
                 ...state,
                 user: {
@@ -44,12 +43,12 @@ export const reducer = (state, action) => {
                     lastName: action.payload.lastName,
                     username: action.payload.username,
                     email: action.payload.email,
-                    password: action.payload.password
-                }
+                    password: action.payload.password,
+                },
             }
 
         default:
-            console.log("Error: No such method in reducer")
-            throw new Error();
+            console.log('Error: No such method in reducer')
+            throw new Error()
     }
 }
