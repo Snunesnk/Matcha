428cdfef0db86e7acc6ee323001c65b3f8def7b7
Simon NUNES <nunesimon85@gmail.com>
2023-09-22 14:45:41 +0200

    Update components

--- front/src/Components/ChatComponent/index.jsx
+++ front/src/Components/ChatComponent/index.jsx
@@ -1,48 +1,46 @@
 import { useEffect, useRef, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Grid } from '@mui/material'
-import ClearIcon from '@mui/icons-material/Clear'
+import { useParams } from 'react-router-dom'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import SendIcon from '@mui/icons-material/Send'
+import PersonIcon from '@mui/icons-material/Person'
 import './index.css'
 
-const ChatComponent = () => {
+const DUMMY_MESSAGES = [
+    {
+        from: 0,
+        content: "What's a girl like you doing on an app like this",
+    },
+    {
+        from: 1,
+        content: 'seeing what I can find',
+    },
+    {
+        from: 1,
+        content: 'lol',
+    },
+    {
+        from: 1,
+        content: 'Who are you?',
+    },
+    {
+        from: 0,
+        content: 'Same question, who are you ???',
+    },
+    {
+        from: 1,
+        content: 'Well,that actually is a good question',
+    },
+    {
+        from: 0,
+        content: 'Glad you acknowlegde it',
+    },
+]
+
+const ChatComponent = ({ user, components, setActiveComponent }) => {
     const params = useParams()
     const [newMessage, setNewMessage] = useState('')
-    const [messages, setMessages] = useState([
-        {
-            from: params.id,
-            content: 'Hey!',
-        },
-        {
-            from: params.id,
-            content: 'How are you?',
-        },
-        {
-            from: params.id,
-            content: 'What are you doing?',
-        },
-        {
-            from: params.id,
-            content: 'Who are you?',
-        },
-        {
-            from: 0,
-            content: 'Same question, who are you ???',
-        },
-        {
-            from: params.id,
-            content: 'Well,that actually is a good question',
-        },
-        {
-            from: 0,
-            content: 'Glad you acknowlegde it',
-        },
-    ])
-    const [person, setPerson] = useState({
-        id: 1,
-        profile_pic: '/src/assets/cat_profile.jpg',
-        username: 'Jon the cat',
-    })
+    const [messages, setMessages] = useState(DUMMY_MESSAGES)
+    const [person, setPerson] = useState(user)
 
     const messageEnd = useRef(null)
     const messageInput = useRef(null)
@@ -70,66 +68,57 @@ const ChatComponent = () => {
     }, [messages])
 
     return (
-        <Grid container item xs={9}>
-            <Grid item xs={6}></Grid>
-            <Grid
-                container
-                item
-                direction="column"
-                justifyContent="space-between"
-                id="messages_container"
-            >
-                <Grid item container id="chat_header">
-                    <Grid item id="person_info">
-                        <div className="chat_img_container_message">
-                            <img src={person.profile_pic}></img>
-                        </div>
-                        <div className="message_username">
-                            {person.username}
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <Link to="/dashboard">
-                            <button className="close_messages_btn">
-                                <ClearIcon />
-                            </button>
-                        </Link>
-                    </Grid>
-                </Grid>
-                <Grid item id="chat_body">
-                    {messages.map((message, i) => (
-                        <div
-                            className={
-                                'messsage_line' +
-                                (message.from == params.id ? ' other' : ' self')
-                            }
-                            key={i}
-                        >
-                            <div className="message">{message.content}</div>
-                        </div>
-                    ))}
-                    <div ref={messageEnd}></div>
-                </Grid>
-                <Grid item id="chat_footer">
-                    <input
-                        id="messages_input"
-                        placeholder="Type your message here"
-                        maxLength="280"
-                        onChange={(e) => setNewMessage(e.target.value)}
-                        value={newMessage}
-                        autoFocus
-                        ref={messageInput}
-                        onKeyUp={(key) => {
-                            if (key.code === 'Enter') sendMessage()
-                        }}
+        <div id="messages_container">
+            <div id="chat_header">
+                <div id="person_info">
+                    <ArrowBackIcon
+                        onClick={() =>
+                            setActiveComponent(components.MESSAGE_LIST)
+                        }
                     />
-                    <SendIcon
-                        className="messages_send_icon"
-                        onClick={sendMessage}
+                    <img src={person.imgA} alt="profile_pic" />
+                    <div className="message_username">{person.login}</div>
+                </div>
+                <div id="user-chat-more">
+                    <PersonIcon
+                        onClick={() =>
+                            setActiveComponent(components.USER_PROFILE)
+                        }
                     />
-                </Grid>
-            </Grid>
-        </Grid>
+                </div>
+            </div>
+            <div id="chat_body">
+                {messages.map((message, i) => (
+                    <div
+                        className={
+                            'messsage_line' +
+                            (message.from == 0 ? ' other' : ' self')
+                        }
+                        key={i}
+                    >
+                        <div className="message">{message.content}</div>
+                    </div>
+                ))}
+            </div>
+            <div id="chat_footer">
+                <input
+                    id="messages_input"
+                    placeholder="Type your message here"
+                    maxLength="280"
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                    autoFocus
+                    ref={messageInput}
+                    onKeyUp={(key) => {
+                        if (key.code === 'Enter') sendMessage()
+                    }}
+                />
+                <SendIcon
+                    className="messages_send_icon"
+                    onClick={sendMessage}
+                />
+            </div>
+        </div>
     )
 }
 
