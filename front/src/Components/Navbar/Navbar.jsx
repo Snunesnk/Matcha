7f5f1dc24e271a9554df7e9b16e625e21a5c383e
Simon NUNES <snunes@veepee.com>
2023-11-25 17:58:00 +0100

    continue on login

--- front/src/Components/Navbar/Navbar.jsx
+++ front/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { AlreadyHaveAccountBtn, LogoutBtn } from '../Button/Button'
+import React, { useEffect } from 'react'
+import { Link, useMatch } from 'react-router-dom'
+import { AlreadyHaveAccountBtn, CreateAccountButton } from '../Button/Button'
 import { useSelector } from 'react-redux'
 import './Navbar.css'
 
 const Navbar = () => {
     const loggedIn = useSelector((state) => state.userState.userStatus.loggedIn)
+    const match = useMatch('/login')
 
     return (
         <div id="navbar">
-            <Link id="nav-title-container" to={loggedIn ? "/dashboard" : "/"}>
+            <Link id="nav-title-container" to={loggedIn ? '/dashboard' : '/'}>
                 <h3 className="navTitle">MatChat</h3>
             </Link>
             <Link to="/dashboard/userSettings">
@@ -19,9 +20,8 @@ const Navbar = () => {
                 <h2 className="onglet">Chat</h2>
             </Link>
             <div className="center log-in-btn-container">
-                {loggedIn ?
-                <LogoutBtn /> : 
-                <AlreadyHaveAccountBtn />}
+                {loggedIn == false && !match && <AlreadyHaveAccountBtn />}
+                {loggedIn == false && match && <CreateAccountButton />}
             </div>
         </div>
     )
--- front/src/Pages/Login/Login.jsx
+++ front/src/Pages/Login/Login.jsx
@@ -2,41 +2,95 @@ import { useState } from 'react'
 import Button from '../../Components/Button/Button'
 import FormInput from '../../Components/FormInput/FormInput'
 import './Login.css'
+import { Form, useActionData } from 'react-router-dom'
+
+// const LoginPage = () => {
+//     const [login, setLogin] = useState('')
+//     const [password, setPassword] = useState('')
+
+//     return (
+//         <div id="login_page">
+//             <div id="login-container">
+//                 <div id="login-welcome">
+//                     <h2>Log in to your account</h2>
+//                     <p>Welcome back! Please enter your details.</p>
+//                 </div>
+//                 <div className="login-input">
+//                     <label htmlFor="login">Login</label>
+//                     <FormInput
+//                         placeholder="Login"
+//                         name="login"
+//                         required={true}
+//                         updateValue={setLogin}
+//                     />
+//                 </div>
+
+//                 <div className="login-input">
+//                     <label htmlFor="password">Password</label>
+//                     <FormInput
+//                         placeholder="Password"
+//                         name="password"
+//                         required={true}
+//                         updateValue={setPassword}
+//                     />
+//                 </div>
+//                 <div id="login-forgot-password">
+//                     <label>Forgot password?</label>
+//                 </div>
+//                 <Button text="Sign in" btnClass="pink" />
+//             </div>
+//         </div>
+//     )
+// }
 
 const LoginPage = () => {
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
+    const formResult = useActionData()
 
     return (
-        <div id="login_page">
-            <div id="login-container">
-                <div id="login-welcome">
-                    <h2>Log into your account</h2>
-                    <p>Welcome back! Please enter your details.</p>
-                </div>
-                <div className="login-input">
-                    <label htmlFor="login">Login</label>
-                    <FormInput
-                        placeholder="Login"
-                        name="login"
-                        required={true}
-                        updateValue={setLogin}
-                    />
-                </div>
-
-                <div className="login-input">
-                    <label htmlFor="password">Password</label>
-                    <FormInput
-                        placeholder="Password"
-                        name="password"
-                        required={true}
-                        updateValue={setPassword}
-                    />
-                </div>
-                <div id="login-forgot-password">
-                    <label>Forgot password?</label>
-                </div>
-                <Button text="Sign in" btnClass="pink" />
+        <div id="test-div">
+            <div id="right-div-t">
+                <div></div>
+            </div>
+            <div id="right-div">
+                <Form id="onboarding_form" method="post">
+                    <div id="login-welcome">
+                        <h2>Log in to your account</h2>
+                        <p>Welcome back! Please enter your details.</p>
+                    </div>
+                    <div>
+                        <FormInput
+                            placeholder="Login"
+                            name="login"
+                            required={true}
+                            updateValue={setLogin}
+                        />
+                    </div>
+                    <div>
+                        <FormInput
+                            placeholder="Password"
+                            name="password"
+                            required={true}
+                            updateValue={setPassword}
+                        />
+                    </div>
+                    {formResult &&
+                        formResult.error !==
+                            validationErrors.noValidationError && (
+                            // <div className="centered_container">
+                            <label className="errorLabel">
+                                {formResult.error}
+                            </label>
+                            // </div>
+                        )}
+                    <div id="login-forgot-password">
+                        <label>Forgot password?</label>
+                    </div>
+                    <button className="btn signup-btn" type="submit">
+                        Sign In
+                    </button>
+                </Form>
             </div>
         </div>
     )
