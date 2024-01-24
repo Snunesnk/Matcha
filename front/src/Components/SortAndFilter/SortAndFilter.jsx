7581ea9b511a6e256cfc9dae88a8276b308ddf5d
Simon NUNES <nunesimon85@gmail.com>
2024-01-24 13:22:03 +0100

    Continue filtering and sorting

--- front/src/Components/SortAndFilter/SortAndFilter.jsx
+++ front/src/Components/SortAndFilter/SortAndFilter.jsx
@@ -2,41 +2,46 @@ import SimpleSelect from '../Select/Select'
 import TagsAutocomplete from '../TagsAutocomplete/TagsAutocomplete'
 import './SortAndFilter.css'
 
-const GENDERS = ['f', 'm', 'nb']
+const SORT_FIELDS = ['Age', 'Distance', 'Popularity', 'Tags']
+const SORT_DIRECTION = ['Asc.', 'Desc.']
+const AGE_FILTER = ['all', '18-25', '25-35', '35-45', '45-55', '55+']
+const LOCATION_FILTER = ['all', '<5km', '<10km', '<25km', '<50km', '50+km']
+const POPULARITY_FILTER = ['all', '100', '>80', '>60', '>40', '>20']
 
 const SortAndFilter = ({ setSort, setFilter, active }) => {
     return (
         <div className={'sort-and-filter ' + (active ? 'active' : 'inactive')}>
             <div className="setting">
                 <div>Sort by</div>
-                <div>
+                <div className="double-select">
                     <SimpleSelect
-                        options={GENDERS.map((gender) => gender)}
+                        options={SORT_FIELDS.map((gender) => gender)}
                         // defaultSelected={userPreferences}
                         // onChange={handlePreferenceChange}
                     />
                     <SimpleSelect
-                        options={GENDERS.map((gender) => gender)}
+                        options={SORT_DIRECTION.map((gender) => gender)}
+                        // defaultSelected={userPreferences}
+                        // onChange={handlePreferenceChange}
+                    />
+                </div>
+            </div>
+            <p>Filters</p>
+            <div className="setting">
+                <div>Age</div>
+                <div>
+                    <SimpleSelect
+                        options={AGE_FILTER.map((gender) => gender)}
                         // defaultSelected={userPreferences}
                         // onChange={handlePreferenceChange}
                     />
                 </div>
             </div>
             <div className="setting">
-                <div>Filter age</div>
+                <div>Location</div>
                 <div>
                     <SimpleSelect
-                        options={GENDERS.map((gender) => gender)}
-                        // defaultSelected={userPreferences}
-                        // onChange={handlePreferenceChange}
-                    />
-                </div>
-            </div>
-            <div className="setting">
-                <div>Filter location</div>
-                <div>
-                    <SimpleSelect
-                        options={GENDERS.map((gender) => gender)}
+                        options={LOCATION_FILTER.map((gender) => gender)}
                         // defaultSelected={userPreferences}
                         // onChange={handlePreferenceChange}
                     />
@@ -44,10 +49,10 @@ const SortAndFilter = ({ setSort, setFilter, active }) => {
             </div>
 
             <div className="setting">
-                <div>Filter popularity</div>
+                <div>Popularity</div>
                 <div>
                     <SimpleSelect
-                        options={GENDERS.map((gender) => gender)}
+                        options={POPULARITY_FILTER.map((gender) => gender)}
                         // defaultSelected={userPreferences}
                         // onChange={handlePreferenceChange}
                     />
@@ -55,7 +60,7 @@ const SortAndFilter = ({ setSort, setFilter, active }) => {
             </div>
 
             <div className="setting complex-setting">
-                <div className="setting-infos">Filter tags</div>
+                <div className="setting-infos">Tags</div>
                 <TagsAutocomplete
                 // value={searchTags}
                 // setValue={(e, tagsList) => setSearchTags(tagsList)}
