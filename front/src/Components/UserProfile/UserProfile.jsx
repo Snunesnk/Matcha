5a161c6741de8cca06d0112780e9d26f48f8b6e9
Kenny DUONG <chikhang.duong@veepee.com>
2024-01-25 01:33:42 +0100

    rating

--- front/src/Components/UserProfile/UserProfile.jsx
+++ front/src/Components/UserProfile/UserProfile.jsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {
     ArrowDropUp, ArrowLeft, ArrowRight, Clear, Place,
     Favorite, FavoriteBorder, Block, ReportOff,
-    Star, StarBorder, StarHalf
+    Star, StarBorder, StarHalf,
+    Female, Male, Transgender
 } from '@mui/icons-material'
 import './UserProfile.css'
 import socket from '../../Socket/socket'
@@ -27,10 +28,34 @@ function calculateAge(birthdate) {
     return age
 }
 
+const StarRating = ({ percentage }) => {
+    const ratingOutOfThree = (percentage / 100) * 3;
+    const fullStars = Math.floor(ratingOutOfThree);
+    const halfStar = ratingOutOfThree % 1 >= 0.5 ? 1 : 0;
+    const emptyStars = 3 - fullStars - halfStar;
+
+    const fullStarsElements = Array(fullStars).fill(<Star />);
+    const halfStarElement = halfStar ? <StarHalf /> : null;
+    const emptyStarsElements = Array(emptyStars).fill(<StarBorder />);
+    const starElements = [...fullStarsElements, halfStarElement, ...emptyStarsElements];
+
+    return <div>{starElements}</div>;
+}
+
+const GenderIcon = ({ gender }) => {
+    switch (gender.toLowerCase()) {
+        case 'f':
+            return <Female fontSize='large' />;
+        case 'm':
+            return <Male fontSize='large' />;
+        default:
+            return <Transgender fontSize='large' />;
+    }
+}
+
 const UserProfile = ({ user, scroll = 0 }) => {
     const [selectedPicture, setSelectedPicture] = useState(-1)
     const [currentOnline, setCurrentOnline] = useState(false)
-    const [like,  setLike] = useState(false)
     const profileRef = useRef(null)
     const infosRef = useRef(null)
     const dispatch = useDispatch()
@@ -95,82 +120,39 @@ const UserProfile = ({ user, scroll = 0 }) => {
 
     return (
         <div id="user-profile-container" className={selectedPicture !== -1 ? 'no-overflow' : ''}>
-            <div className="card_img_container"
-                style={{
-                    background: 'url('
-                        + (user.imgA?.includes('http') ? '' : 'http://localhost:8080/api')
-                        + user.imgA + ') 50% 50% / cover no-repeat',
-                }}>
+            <div className="card_img_container" style={{ background: 'url(' + (user.imgA?.includes('http') ? '' : 'http://localhost:8080/api') + user.imgA + ') 50% 50% / cover no-repeat' }}>
                 <div className="name_and_age_container">
-                    <div className='info-row'>
-                        <div className='info-rating'>
-                            <button className="info-chip" onClick={toggleScroll}>
-                                {scroll <= 50 ? 'Info' : <ArrowDropUp />}
-                            </button>
-                            
-                            <Star/><StarHalf/><StarBorder/>
+
+                    <button className="info-chip" onClick={toggleScroll}>
+                        {scroll <= 50 ? 'Info' : <ArrowDropUp />}
+                    </button>
+
+                    <div className="main-info">
+                        <div ref={profileRef} className="name_and_age">
+                            {user.surname}, {userAge} <GenderIcon gender={user.gender} />
                         </div>
-
-                        <div className='like-block'>
-                            <Block fontSize='large' />
-                            {like ? <Favorite fontSize='large' /> : <FavoriteBorder fontSize='large' />}
-                        </div>
+                        <StarRating percentage={50} />
                     </div>
 
-                    <div className="info-chip-container">
-                        <button className="info-chip" onClick={toggleScroll}>
-                            {scroll <= 50 ? 'Info' : <ArrowDropDownIcon />}
-                        </button>
-                    </div>
-                    
-                    <div className='icons-bar'>
-                        <button className="info-chip" onClick={toggleScroll}>
-                            {scroll <= 50 ? 'Info' : <ArrowDropUp />}
-                        </button>
-
-                        <div className='like-block'>
-                            <Block fontSize='large' />
-                            {like ? <Favorite fontSize='large' /> : <FavoriteBorder fontSize='large' />}
-                        </div>
-                    </div>
-
-                    <div ref={profileRef} className="name_and_age">
-                        {user.surname}, {userAge}
-                    </div>
-                    <div className="connected-and-login"></div>
                     <div ref={infosRef} className="user-location-infos">
                         <div className="location">
-                            <PlaceIcon /> {Math.floor(user.distance)}km away
+                            <Place /> {Math.floor(user.distance)}km away
                             <div className="indicator-container">
-                                <div
-                                    className={
-                                        'indicator' +
-                                        (currentOnline.online
-                                            ? ' online'
-                                            : ' offline')
-                                    }
-                                ></div>
+                                <div className={'indicator' + (currentOnline.online ? ' online' : ' offline')}></div>
                                 <p className="last-online">
-                                    {currentOnline.online
-                                        ? 'Connected'
-                                        : formatTimeDifference(
-                                              currentOnline.lastOnline
-                                          )}
+                                    {currentOnline.online ? 'Connected' : formatTimeDifference(currentOnline.lastOnline)}
                                 </p>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
 
             <div id="user-profile-infos">
-                {user.surname} {user.name}, {userAge}{' '}
-                <span>{user.gender.toUpperCase()}</span>
+                {user.surname} {user.name}
+                <i id="user-login">@{user.login}</i>
             </div>
-            <div className="user-login-container">
-                <i id="user-login"> @{user.login}</i>
-            </div>
+
             <div id="user-profile-bio">
                 <b>About me</b>
                 {user.bio}
@@ -178,11 +160,20 @@ const UserProfile = ({ user, scroll = 0 }) => {
 
             <div id="user-profile-pictures-container">
                 {imgs.map((img, i) => (
-                    <div className="user-profile-picture"
+                    <div
                         key={i}
-                        style={{ background: 'url(' + (!img || img.includes('http') ? '' : 'http://localhost:8080/api') + img + ') center', }}
-                        onClick={() => setSelectedPicture(i)}>
-                    </div>
+                        className="user-profile-picture"
+                        style={{
+                            background:
+                                'url(' +
+                                (!img || img.includes('http')
+                                    ? ''
+                                    : 'http://localhost:8080/api') +
+                                img +
+                                ') center',
+                        }}
+                        onClick={() => setSelectedPicture(i)}
+                    ></div>
                 ))}
             </div>
 
@@ -204,10 +195,15 @@ const UserProfile = ({ user, scroll = 0 }) => {
                         </div>
                     ))}
             </div>
-            <div id="display-user-picture"
-                className={'card_img_container' + (selectedPicture === -1 ? ' hidden' : '')}
+            <div
+                id="display-user-picture"
+                className={
+                    'card_img_container' +
+                    (selectedPicture === -1 ? ' hidden' : '')
+                }
                 style={{
-                    background: 'url(' +
+                    background:
+                        'url(' +
                         (selectedPicture >= imgs.length ||
                             selectedPicture == -1 ||
                             imgs[selectedPicture].indexOf('http') > -1
@@ -215,22 +211,38 @@ const UserProfile = ({ user, scroll = 0 }) => {
                             : 'http://localhost:8080/api') +
                         imgs[selectedPicture] +
                         ') 50% 50% / cover no-repeat',
-                }}>
+                }}
+            >
                 <div id="picture-navigation">
-                    <div className="user-picture-nav user-picture-prev" onClick={prevPicture}>
+                    <div
+                        className="user-picture-nav user-picture-prev"
+                        onClick={prevPicture}
+                    >
                         <ArrowLeft />
                     </div>
                     {imgs.map((img, i) => (
-                        <div className={'img-indicator' + (selectedPicture === i ? ' img-selected' : '')}
+                        <div
+                            className={
+                                'img-indicator' +
+                                (selectedPicture === i ? ' img-selected' : '')
+                            }
                             key={i}
-                            onClick={() => { setSelectedPicture(i) }}>
-                        </div>
+                            onClick={() => {
+                                setSelectedPicture(i)
+                            }}
+                        ></div>
                     ))}
-                    <div className="user-picture-nav user-picture-next" onClick={nextPicture}>
+                    <div
+                        className="user-picture-nav user-picture-next"
+                        onClick={nextPicture}
+                    >
                         <ArrowRight />
                     </div>
                 </div>
-                <button id="btn-close-magnify-picture" onClick={() => setSelectedPicture(-1)}>
+                <button
+                    id="btn-close-magnify-picture"
+                    onClick={() => setSelectedPicture(-1)}
+                >
                     <Clear />
                 </button>
             </div>
