c1bd87841c6895d10337eea95e8a15e7384818b2
Simon NUNES <nunesimon85@gmail.com>
2024-01-25 21:50:26 +0100

    Allowed image removal on mobile click

--- front/src/Components/ImageUpload/index.jsx
+++ front/src/Components/ImageUpload/index.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './index.css'
 import '../Settings/Settings.css'
 
-const ImageUpload = ({ defaultImages = [], setFileList = () => { } }) => {
+const ImageUpload = ({ defaultImages = [], setFileList = () => {} }) => {
     const [imgs, setImgs] = useState(defaultImages)
+    const [pictureClicked, setPictureClicked] = useState(-1)
+    const imgRef = useRef()
 
     useEffect(() => {
         setImgs(defaultImages)
     }, [defaultImages])
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (imgRef.current && !imgRef.current.contains(event.target)) {
+                setPictureClicked(-1)
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [imgRef])
 
     const onChange = (e) => {
         const fileList = e.target.files
@@ -30,6 +45,7 @@ const ImageUpload = ({ defaultImages = [], setFileList = () => { } }) => {
         setImgs((prev) => {
             return prev.filter((i) => i !== img)
         })
+        setPictureClicked(-1)
     }
 
     return (
@@ -37,30 +53,61 @@ const ImageUpload = ({ defaultImages = [], setFileList = () => { } }) => {
             <div className="setting-infos">
                 <div>Your pictures</div>
                 <div>
-                    <label htmlFor="picture_upload_btn" id="upload_pictures_btn_label" className={imgs.length >= 5 ? 'disabled' : ''}>
+                    <label
+                        htmlFor="picture_upload_btn"
+                        id="upload_pictures_btn_label"
+                        className={imgs.length >= 5 ? 'disabled' : ''}
+                    >
                         Upload
                     </label>
                 </div>
             </div>
 
-            {imgs.length === 0 && (<p>Add pictures to show everyone how beautiful you are !</p>)}
+            {imgs.length === 0 && (
+                <p>Add pictures to show everyone how beautiful you are !</p>
+            )}
 
-            <div id="user-settings-pictures-container">
-                {imgs.length > 0 && imgs.map((img, i) => {
-                    // Create URL if it not a URL
-                    const imgUrl = img.name !== undefined ? URL.createObjectURL(img) : img.includes('http') ? img : 'http://localhost:8080/api' + img
+            <div id="user-settings-pictures-container" ref={imgRef}>
+                {imgs.length > 0 &&
+                    imgs.map((img, i) => {
+                        // Create URL if it not a URL
+                        const imgUrl =
+                            img.name !== undefined
+                                ? URL.createObjectURL(img)
+                                : img.includes('http')
+                                ? img
+                                : 'http://localhost:8080/api' + img
 
-                    return (
-                        <div className="setting-picture-container" key={i}>
-                            <div className="user-setting-picture" style={{ background: 'url(' + imgUrl + ')', }}></div>
-                            <div className="setting-picture-delete">
-                                <button className="setting-picture-delete-btn" onClick={() => removeImg(img)}>
-                                    Delete
-                                </button>
+                        return (
+                            <div
+                                className={
+                                    'setting-picture-container' +
+                                    (pictureClicked === i ? ' selected' : '')
+                                }
+                                key={i}
+                                onClick={() => {
+                                    setPictureClicked(i)
+                                }}
+                            >
+                                <div
+                                    className="user-setting-picture"
+                                    style={{
+                                        background: 'url(' + imgUrl + ')',
+                                    }}
+                                ></div>
+                                <div className="setting-picture-delete">
+                                    <button
+                                        className="setting-picture-delete-btn"
+                                        onClick={() => {
+                                            removeImg(img)
+                                        }}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
             </div>
 
             <input
