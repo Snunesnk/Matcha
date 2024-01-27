1385abb64ca5a76a8b969248eed548324e853ffe
Simon NUNES <nunesimon85@gmail.com>
2024-01-27 17:07:30 +0100

    Removed trailing state

--- front/src/Components/OnboardingFormComponents/AllSetMessage.jsx
+++ front/src/Components/OnboardingFormComponents/AllSetMessage.jsx
@@ -43,7 +43,6 @@ const AllSetMessage = () => {
                                 )
                                 break
                         }
-                        setLoading(false)
                     })
             })
             .catch((error) => {
