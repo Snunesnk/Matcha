eee48431f3fad0c2bcc8fff7cb5dd1facf805107
snunes <snunes@student.42.fr>
2024-01-28 22:01:10 +0000

    pushed route and handler

---
+++ server/app/handlers/blocked.handler.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export default class {
+  static validate = async (req, res) => {
+    const token = req.cookies.remember_me;
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+      if (err) {
+        const user = { loggedIn: false, onboarded: false, verified: false };
+        res.json(user);
+      } else {
+        const user = await User.getUserByLogin(decoded.login);
+        if (!user) {
+          return res.status(401).send({ message: "Unauthorized!" });
+        }
+
+        // Issue new token for sliding session
+        const newToken = await authenticationService.generateToken(user);
+        res.cookie("remember_me", newToken, {
+          maxAge: 1000 * 60 * 15,
+          httpOnly: true,
+          path: "/",
+          sameSite: "strict",
+        });
+
+        res.json({
+          loggedIn: true,
+          onboarded: user.onboarded || false,
+          verified: user.verified || false,
+        });
+      }
+    });
+  };
+}
---
+++ server/app/routes/blocked.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+
+// Get an image
+router.post("/blocked", );
+
+export default router;
